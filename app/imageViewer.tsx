717330477083c63d3a96afd226ae8da928289bc5
Aspharier <codeworkashish@gmail.com>
2025-01-29 01:07:27 +0530

    Modified the Modal Screen

--- app/imageViewer.tsx
+++ app/imageViewer.tsx
@@ -1,21 +1,46 @@
-import { Modal, View, Text, StyleSheet, Pressable } from "react-native";
+import {
+  Modal,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
 import { PropsWithChildren } from "react";
-
+import { Image } from "expo-image";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import { RobotoMono_300Light, useFonts } from "@expo-google-fonts/roboto-mono";
+
+const placeHolderImage = require("../assets/images/sample3.png");
 
 type Props = PropsWithChildren<{
   isVisible: boolean;
-  onClose(): () => void;
+  onClose(): void;
 }>;
 
 export default function ImageViewer({ isVisible, onClose }: Props) {
+  const [loaded] = useFonts({
+    RobotoMono_300Light,
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Modal animationType="slide" transparent={true} visible={isVisible}>
       <View style={styles.modalContainer}>
-        <Pressable onPress={onClose}>
-          <AntDesign name="closecircleo" size={24} color="white" />
-        </Pressable>
-        <Text>Hello From the MOdal itself</Text>
+        <View style={styles.modalContent}>
+          <Pressable onPress={onClose} style={styles.closeButton}>
+            <AntDesign name="closecircleo" size={40} color="#FBFDFC" />
+          </Pressable>
+          <View style={styles.imageContainer}>
+            <Image source={placeHolderImage} style={styles.image} />
+          </View>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Enhance</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Modal>
   );
@@ -23,12 +48,42 @@ export default function ImageViewer({ isVisible, onClose }: Props) {
 
 const styles = StyleSheet.create({
   modalContainer: {
-    height: "70%",
+    flex: 1,
+    backgroundColor: "#151618",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "90%",
+    maxWidth: 500,
+    alignItems: "center",
+  },
+  closeButton: {
+    alignSelf: "flex-end",
+    marginBottom: 10,
+  },
+  imageContainer: {
     width: "100%",
-    backgroundColor: "#25292e",
-    borderTopRightRadius: 18,
-    borderTopLeftRadius: 18,
-    position: "absolute",
-    bottom: 0,
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 20,
+  },
+  buttonContainer: {
+    backgroundColor: "#72D3FE",
+    borderRadius: 20,
+    alignItems: "center",
+    paddingVertical: 10,
+    width: "100%",
+    marginTop: 20,
+  },
+  buttonText: {
+    fontFamily: "RobotoMono_300Light",
+    fontSize: 30,
+    color: "#151618",
   },
 });
